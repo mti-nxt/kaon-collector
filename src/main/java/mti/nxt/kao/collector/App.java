a25c4994bcff8801a62bd00a4d14c22fb8361991
masakkuma <golden.forest627@gmail.com>
2016-07-03 22:18:06 +0900

    first commit

---
+++ src/main/java/mti/nxt/kao/collector/App.java
@@ -0,0 +1,14 @@
+package mti.nxt.kao.collector;
+
+/**
+ * Created by kanamori_m on 2016/07/03.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Start kao collector");
+
+    }
+
+}
