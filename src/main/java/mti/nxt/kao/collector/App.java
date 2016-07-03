6e28698be40eae7f8956006fbc11eac4f5168760
masakkuma <golden.forest627@gmail.com>
2016-07-04 07:45:25 +0900

    画像URLの取得まで

--- src/main/java/mti/nxt/kao/collector/App.java
+++ src/main/java/mti/nxt/kao/collector/App.java
@@ -1,5 +1,8 @@
 package mti.nxt.kao.collector;
 
+
+import twitter4j.*;
+
 /**
  * Created by kanamori_m on 2016/07/03.
  */
@@ -8,6 +11,36 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Start kao collector");
+
+        Twitter twitter = TwitterFactory.getSingleton();
+
+        try {
+            // ユーザーの表示名からユーザーIDの取得
+            User user = twitter.showUser("@masaosaan");
+            System.out.println(user.getId());
+            final long id = user.getId();
+
+            final ResponseList<Status> userTimeline = twitter.getUserTimeline(id);
+
+
+            System.out.println(userTimeline.size());
+
+            userTimeline.forEach(tweet -> {
+                System.out.println("tweet:--------------");
+                System.out.println(tweet.getText());
+                System.out.println("--------------------");
+                final MediaEntity[] mediaEntities = tweet.getMediaEntities();
+                for (MediaEntity media:mediaEntities) {
+                    System.out.println("Media=========");
+                    System.out.println(media.getMediaURL());
+                }
+            });
+
+
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
